28cddae2ea58620afbbebfa90fccbde7abdedb9d
Will <willianwed98@gmail.com>
2020-06-01 15:12:50 -0300

    Generics2

---
+++ Generics2/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import services.CalculationService;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		String path = "C:\\Users\\willi\\Documents\\in.txt";
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			String line = br.readLine();
+			while(line != null) {
+				String[] fields = line.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				list.add(new Product(name, price));
+				line = br.readLine();
+			}
+			
+			Product x = CalculationService.max(list);
+			System.out.println("Most expesive: ");
+			System.out.println(x);
+		}
+		catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
